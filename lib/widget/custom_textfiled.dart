b9e75e7535b89be436ec67be142618170ebfae0a
parthunitalks <parthdomadiya21@gmail.com>
2023-01-06 10:29:11 +0530

    ios textfieald change

--- lib/widget/custom_textfiled.dart
+++ lib/widget/custom_textfiled.dart
@@ -26,40 +26,37 @@ class CustomTextField extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 48,
-      width: Get.width,
-      decoration: BoxDecoration(
-        color: Theme.of(context).appBarTheme.backgroundColor,
-        image: DecorationImage(
-          image: AssetImage(
-            AppTheme.isLightTheme == true
-                ? DefaultImages.borderBG
-                : DefaultImages.darkInput,
-          ),
-        ),
-      ),
-      child: Padding(
-        padding: const EdgeInsets.only(left: 16, right: 16),
-        child: TextFormField(
-          keyboardType: inputType,
-          obscureText: isObsecure,
-          controller: textEditingController,
-          style: Theme.of(context).textTheme.bodyText1!.copyWith(
+      height: 50,
+      // decoration: BoxDecoration(
+      //   color: Theme.of(context).appBarTheme.backgroundColor,
+      //   image: DecorationImage(
+      //     image: AssetImage(
+      //       AppTheme.isLightTheme == true
+      //           ? DefaultImages.borderBG
+      //           : DefaultImages.darkInput,
+      //     ),
+      //   ),
+      // ),
+      child: TextFormField(
+        keyboardType: inputType,
+        obscureText: isObsecure,
+        controller: textEditingController,
+        style: Theme.of(context).textTheme.bodyText1!.copyWith(
+              fontSize: 16,
+              fontWeight: FontWeight.w600,
+            ),
+        decoration: InputDecoration(
+          contentPadding: const EdgeInsets.only(top: 13, left: 14, right: 14),
+          // border: InputBorder.none,
+          border: OutlineInputBorder(borderRadius: BorderRadius.circular(20)),
+          suffixIcon: sufix ?? const SizedBox(),
+          prefixIcon: prefix ?? null,
+          hintText: hintText,
+          hintStyle: Theme.of(context).textTheme.bodyText1!.copyWith(
                 fontSize: 16,
-                fontWeight: FontWeight.w600,
+                fontWeight: FontWeight.w400,
+                color: HexColor(AppTheme.secondaryColorString!),
               ),
-          decoration: InputDecoration(
-            contentPadding: const EdgeInsets.only(top: 13, left: 14, right: 14),
-            border: InputBorder.none,
-            suffixIcon: sufix ?? const SizedBox(),
-            prefixIcon: prefix ?? null,
-            hintText: hintText,
-            hintStyle: Theme.of(context).textTheme.bodyText1!.copyWith(
-                  fontSize: 16,
-                  fontWeight: FontWeight.w400,
-                  color: HexColor(AppTheme.secondaryColorString!),
-                ),
-          ),
         ),
       ),
     );
